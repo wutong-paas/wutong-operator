ea0fe3f34d1cc4787a71edc063fb3f5d7aca94f3
poneding <poneding@gmail.com>
2023-07-07 16:44:26 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@
 
 export NAMESPACE=wutong-operator
 export VERSION=v1.4.0
-# docker buildx create --use --name operatorbuilder
-docker buildx use operatorbuilder
+docker buildx create --use --name operatorbuilder || docker buildx use operatorbuilder
 # docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
 docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
 # docker buildx rm operatorbuilder
