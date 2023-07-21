59406d82c6ad0b3b1e0fa862f68a03c28a4cfdbc
poneding <poneding@gmail.com>
2023-07-21 09:34:14 +0800

    update build way

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o bin/amd64/ma
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 GO111MODULE=on go build -a -o bin/arm64/manager main.go
 
 docker buildx use operatorbuilder || docker buildx create --use --name operatorbuilder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.local . 
 # docker buildx rm operatorbuilder
