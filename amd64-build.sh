0de5d65d2e804d42651304c8fad0b8b6243b8079
poneding <poneding@gmail.com>
2023-07-28 16:33:42 +0800

    update ci and build script

--- amd64-build.sh
+++ amd64-build.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-export NAMESPACE=wutong-operator
+export IMAGE_REGISTRY=swr.cn-southwest-2.myhuaweicloud.com/wutong
+export IMAGE_NAME=wutong-operator
 export VERSION=v1.4.0-amd64
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ./bin/amd64/manager main.go
 
-docker build . -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.local
-docker push swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION}
+docker build . -t ${IMAGE_REGISTRY}/${IMAGE_NAME}:${VERSION} -f Dockerfile.local
+docker push ${IMAGE_REGISTRY}/${IMAGE_NAME}:${VERSION}
 
 rm -rf ./bin/*
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-export NAMESPACE=wutong-operator
+export IMAGE_REGISTRY=swr.cn-southwest-2.myhuaweicloud.com/wutong
+export IMAGE_NAME=wutong-operator
 export VERSION=v1.4.0
 
 go mod download
@@ -9,7 +10,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o bin/amd64/manager main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -a -o bin/arm64/manager main.go
 
 docker buildx use gobuilder || docker buildx create --use --name gobuilder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.local . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ${IMAGE_REGISTRY}/${IMAGE_NAME}:${VERSION} -f Dockerfile.local . 
 # docker buildx rm gobuilder
 
 rm -rf ./bin/*
